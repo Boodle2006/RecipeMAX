699b61f26b491760d1f7fbd10a40404575a35a82
Ben Goldston <bgoldston.2024@saintannsny.org>
2021-07-14 13:51:36 -0400

    Initial Commit

---
+++ RecipeListApp/RecipeListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListAppApp.swift
+//  RecipeListApp
+//
+//  Created by James Goldston on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
